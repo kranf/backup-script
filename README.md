fa68d7b0706bf95b09ce3c97917fee62de056d6e
fr6nck <fr6nck@me.org>
2021-05-24 22:30:30 +0200

    initial commit

--- README.md
+++ README.md
@@ -1,3 +1,17 @@
-backup local files to destination.
+# backup local files to destination.
 
 Only incremental diff from previous backup is copied.
+
+Each backup has a date: ```directory-yyyyMMddTHHmm```
+
+```directory-current``` repertory is a link pointing to the latest backup.
+
+New backup hard-links non modified files to ```directory-current```
+
+## How to use
+
+'''backup.sh -i include.lst [-e exclude.lst] -d destdir
+  -h    print usage
+  -i    path to the list of directories to backup
+  -e    path the list of excluded patterns as supported by rsync
+  -d    path the backup destination dir'''
---
+++ backup.sh
@@ -0,0 +1,72 @@
+#!/usr/bin/bash
+
+# DEST_DIR=$1
+
+# EXCLUDED_PATTERN="**.NEF"
+
+function get_date_formatted {
+  echo $(date +%Y%m%dT%H%M%S)
+}
+
+function usage () {
+  echo "./$(basename $0) -i include.lst [-e exclude.lst] -d destdir"
+  echo "  -h    print usage"
+  echo "  -i    path to the list of directories to backup"
+  echo "  -e    path the list of excluded patterns as supported by rsync"
+  echo "  -d    path the backup destination dir"
+}
+
+while getopts "hi:e:d:" args; do
+  case "${args}" in
+    h)
+      usage
+      exit 0
+      ;;
+    i)
+      INCLUDED_FILES=${OPTARG}
+      ;;
+    e)
+      EXCLUDED_PATTERNS=$(realpath ${OPTARG})
+      ;;
+    d)
+      DEST_DIR=${OPTARG}
+      ;;
+    :)
+      usage
+      exit 1
+      ;;
+    ?)
+      usage
+      exit 1
+      ;;
+  esac
+done
+
+mapfile -t dir_list < "${INCLUDED_FILES}"
+cd ${DEST_DIR}  # every reference to backup files are related to .
+
+for source in "${dir_list[@]}"; do
+  source_basename=$(basename $source)
+  source_dated="$source_basename-$(get_date_formatted)"
+  source_ref="$source_basename-current"
+  link_dest=$(realpath .)/$source_ref
+
+  echo "*****************************************************"
+  echo -e "*** Starting backup of $source"
+  echo "*****************************************************"
+
+  rsync \
+    --verbose \
+    --recursive \
+    --progress \
+    --partial \
+    --human-readable \
+    --times \
+    --links \
+    --link-dest=$link_dest \
+    --exclude-from=$EXCLUDED_PATTERNS \
+    $source/ \
+    $source_dated
+
+    ln -sfn $source_dated $source_ref
+done
